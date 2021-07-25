da6b272625d886aadaffa3d97a2286b53493bcf3
Abdesol <abdesoltak@gmail.com>
2021-07-25 21:40:21 +0300

    Add project files.

---
+++ CutCode/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CutCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void ChangeWindowPosition(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        public void MaxChangeWindowPosition(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                WindowState = WindowState.Normal;
+                DragMove();
+            }
+        }
+
+        private void MinimizeClicked(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void MaximizeClicked(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+    }
+}
