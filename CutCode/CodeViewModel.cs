e292f518f34f1d93cc79a39269463a4b1fc41772
Abdesol <abdesoltak@gmail.com>
2021-08-03 12:44:39 +0300

    Added code view model

---
+++ CutCode/CodeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CutCode
+{
+    public class CodeViewModel : Screen
+    {
+        private readonly IThemeService themeService;
+        public CodeViewModel(IThemeService _themeService)
+        {
+            themeService = _themeService;
+        }
+    }
+}
