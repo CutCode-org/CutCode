3c29a680b5f5a2869e251dd8a87e60f6f2d31501
Abdesol <abdesoltak@gmail.com>
2021-09-02 15:08:08 +0300

    deleted useless folders

---
+++ CutCode.Mobile/CutCode.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CutCode.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CutCode.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CutCode.Mobile/CutCode.Mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CutCode.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CutCode.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
