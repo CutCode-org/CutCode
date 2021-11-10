05299598bbb562b5543a753556a961ca6378b1d1
Piero Castillo <pierocastillo2005@gmail.com>
2021-11-09 23:13:03 -0500

    MainWindow.xaml ported

--- CutCode.CrossPlatform/Controllers/WindowButtons.axaml.cs
+++ CutCode.CrossPlatform/Controllers/WindowButtons.axaml.cs
@@ -1,65 +1,115 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Runtime.InteropServices;
 using Aura.UI.Extensions;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
 using Avalonia.Data;
+using Avalonia.Input;
 using Avalonia.Media;
+using Avalonia.VisualTree;
 
 namespace CutCode.CrossPlatform.Controllers
 {
+    //maximizePath = Geometry.Parse("F1M50,-150L50,-50 150,-50 150,-150 50,-150z M37.5,-162.5L162.5,-162.5 162.5,-37.5 37.5,-37.5 37.5,-162.5z");
+    //restorePath = Geometry.Parse("F1M50,-125L50,-37.5 137.5,-37.5 137.5,-125 50,-125z M75,-150L75,-137.5 150,-137.5 150,-62.5 162.5,-62.5 162.5,-150 75,-150z M62.5,-162.5L175,-162.5 175,-50 150,-50 150,-25 37.5,-25 37.5,-137.5 62.5,-137.5 62.5,-162.5z");
+
+    [PseudoClasses(":minimized", ":normal", ":maximized")]
     public class WindowButtons : TemplatedControl
     {
-        private Button minButton;
-        private Button mediumButton;
-        private Button closeButton;
+        private CompositeDisposable? _disposables;
+
         public WindowButtons()
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            this.GetObservable(HostWindowProperty).Subscribe(w =>
             {
-                IsVisible = false;
+                Detach();
+                if(w is not null)
+                {
+                    Attach(w);
+                }
+            });
+        }
+
+        public virtual void Attach(Window hostWindow)
+        {
+            if (_disposables == null)
+            {
+                HostWindow = hostWindow;
+
+                _disposables = new CompositeDisposable
+                {
+                    HostWindow.GetObservable(Window.WindowStateProperty)
+                    .Subscribe(x =>
+                    {
+                        PseudoClasses.Set(":minimized", x == WindowState.Minimized);
+                        PseudoClasses.Set(":normal", x == WindowState.Normal);
+                        PseudoClasses.Set(":maximized", x == WindowState.Maximized);
+                        PseudoClasses.Set(":fullscreen", x == WindowState.FullScreen);
+                    })
+                };
+
+                Console.WriteLine("attached");
             }
+        }
+
+
+        public virtual void Detach()
+        {
+            if (_disposables != null)
+            {
+                _disposables.Dispose();
+                _disposables = null;
+
+                HostWindow = null;
+            }
+        }
+
+        protected virtual void OnClose()
+        {
+            Console.WriteLine("closed");
+            HostWindow.Close();
+        }
+
+        protected virtual void OnRestore()
+        {
+            Console.WriteLine("restored/maximized");
+            HostWindow.WindowState = HostWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        protected virtual void OnMinimize()
+        {
+            Console.WriteLine("minimized");
+            HostWindow.WindowState = WindowState.Minimized;
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
         {
             base.OnApplyTemplate(e);
-            
-            this.GetControl(e, "PART_MinimizeButton", out minButton);
-            this.GetControl(e, "PART_MediumButton", out mediumButton);
-            this.GetControl(e, "PART_CloseButton", out closeButton);
 
-            minButton.Click += (s, e) =>
-            {
-                WindowState = WindowState.Minimized;
-            };
-            
-            mediumButton.Click += (s,e) =>
-            {
-                WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
-            };
+            var closeButton = e.NameScope.Get<Panel>("PART_CloseButton");
+            var restoreButton = e.NameScope.Get<Panel>("PART_RestoreButton");
+            var minimiseButton = e.NameScope.Get<Panel>("PART_MinimiseButton");
 
-            closeButton.Click += (s, e) =>
-            {
-                Window.Close();
-            };
+            closeButton.AddHandler(PointerReleasedEvent, (sender, e) => OnClose());
+
+            restoreButton.AddHandler(PointerReleasedEvent, (sender, e) => OnRestore());
+
+            minimiseButton.AddHandler(PointerReleasedEvent, (sender, e) => OnMinimize());
         }
-
-        public Window Window
+        public Window HostWindow
         {
-            get => GetValue(WindowProperty);
-            set => SetValue(WindowProperty, value);
+            get => GetValue(HostWindowProperty);
+            set => SetValue(HostWindowProperty, value);
         }
 
-        public static readonly StyledProperty<Window> WindowProperty =
-            AvaloniaProperty.Register<WindowButtons, Window>(nameof(Window), defaultBindingMode: BindingMode.OneWay);
+        public static readonly StyledProperty<Window> HostWindowProperty =
+            AvaloniaProperty.Register<WindowButtons, Window>(nameof(HostWindow));
 
-        public WindowState WindowState
-        {
-            get => GetValue(WindowStateProperty);
-            set => SetValue(WindowStateProperty, value);
-        }
-        public static readonly StyledProperty<WindowState> WindowStateProperty =
-            AvaloniaProperty.Register<WindowButtons, WindowState>(nameof(WindowState), WindowState.Normal, defaultBindingMode: BindingMode.TwoWay);
     }
 }
