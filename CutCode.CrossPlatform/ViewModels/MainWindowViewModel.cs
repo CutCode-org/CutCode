7586ca409937fd056d5bf61c34fa26a58406ae36
Piero Castillo <pierocastillo2005@gmail.com>
2021-11-11 17:25:56 -0500

    add common base for ViewModels

--- CutCode.CrossPlatform/ViewModels/MainWindowViewModel.cs
+++ CutCode.CrossPlatform/ViewModels/MainWindowViewModel.cs
@@ -1,67 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
-using Avalonia;
-using Avalonia.Controls.Templates;
 using Avalonia.Media;
-using Avalonia.Platform;
-using CutCode.CrossPlatform.Interfaces;
-using CutCode.CrossPlatform.Models;
 using ReactiveUI;
-using Avalonia.Media;
 using Avalonia.Media.Imaging;
-using CutCode.DataBase;
 
 namespace CutCode.CrossPlatform.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : PageBaseViewModel
     {
-        private ThemeService themeService;
-        private readonly IAssetLoader assetLoader;
-        public MainWindowViewModel()
+        protected override void OnLightThemeIsSet()
         {
-            assetLoader = AvaloniaLocator.CurrentMutable.GetService<IAssetLoader>();
-            
-            themeService = AvaloniaLocator.CurrentMutable.GetService<ThemeService>();
-            themeService.ThemeChanged += ThemeChanged;
-            
-            var database = AvaloniaLocator.CurrentMutable.GetService<DataBaseManager>();
-            themeService.IsLightTheme = database.isLightTheme;
-        }
-        
-        private void ThemeChanged(object sender, EventArgs e)
-        {
-            windowsBtnColor = themeService.IsLightTheme ? Color.Parse("#090909") : Color.Parse("#F2F2F2");
-            backgroundColor = themeService.IsLightTheme ? Color.Parse("#FCFCFC") : Color.Parse("#36393F");
-            titleBarColor = themeService.IsLightTheme ? Color.Parse("#E3E5E8") : Color.Parse("#202225");
-            SideBarColor = themeService.IsLightTheme ? Color.Parse("#F2F3F5") : Color.Parse("#2A2E33");
-            mainTextColor = themeService.IsLightTheme ? Color.Parse("#0B0B13") : Color.Parse("#94969A");
+            windowsBtnColor = Color.Parse("#090909");
+            backgroundColor = Color.Parse("#FCFCFC");
+            titleBarColor = Color.Parse("#E3E5E8");
+            SideBarColor = Color.Parse("#F2F3F5");
+            mainTextColor = Color.Parse("#0B0B13");
 
-            titlebarBtnsHoverColor = themeService.IsLightTheme ? Color.Parse("#D0D1D2") : Color.Parse("#373737");
+            titlebarBtnsHoverColor = Color.Parse("#D0D1D2");
         }
 
-        private IImage ImageFromUri(string path)
+        protected override void OnDarkThemeIsSet()
         {
-            var uri = new Uri(path);
-            return assetLoader.Exists(uri) ? new Bitmap(assetLoader.Open(uri)) : throw new Exception("WTF");
-        }
+            windowsBtnColor = Color.Parse("#F2F2F2");
+            backgroundColor = Color.Parse("#36393F");
+            titleBarColor = Color.Parse("#202225");
+            SideBarColor = Color.Parse("#2A2E33");
+            mainTextColor = Color.Parse("#94969A");
 
-        #region Pages
-
-        private HomeViewModel _HomeViewModel;
-
-        public HomeViewModel HomeViewModel
-        {
-            get => _HomeViewModel;
-            set => this.RaiseAndSetIfChanged(ref _HomeViewModel, value);
-        }
-        #endregion
-
-        public ThemeService ThemeService
-        {
-            get => themeService;
-            set => this.RaiseAndSetIfChanged(ref themeService, value);
+            titlebarBtnsHoverColor = Color.Parse("#373737");
         }
         
         #region Color
---
+++ CutCode.CrossPlatform/ViewModels/PageBaseViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Avalonia;
+using Avalonia.Media;
+using Avalonia.Platform;
+using CutCode.CrossPlatform.Interfaces;
+using CutCode.DataBase;
+using ReactiveUI;
+
+namespace CutCode.CrossPlatform.ViewModels
+{
+    public class PageBaseViewModel : ViewModelBase
+    {
+        public PageBaseViewModel()
+        {
+            ThemeService = AvaloniaLocator.CurrentMutable.GetService<ThemeService>();
+            DataBase = AvaloniaLocator.CurrentMutable.GetService<DataBaseManager>();
+            AssetLoader = AvaloniaLocator.CurrentMutable.GetService<IAssetLoader>();
+
+            ThemeService.ThemeChanged += (s, e) =>
+            {
+                OnThemeChanged();
+            };
+            ThemeService.IsLightTheme = DataBase.isLightTheme;
+        }
+        
+        protected virtual void OnThemeChanged()
+        {
+            if (ThemeService.IsLightTheme)
+            {
+                OnLightThemeIsSet();
+            }
+            else
+            {
+                OnDarkThemeIsSet();
+            }
+        }
+
+        protected virtual void OnLightThemeIsSet()
+        {
+            
+        }
+
+        protected virtual void OnDarkThemeIsSet()
+        {
+            
+        }
+
+        protected IAssetLoader AssetLoader
+        {
+            get;
+            set;
+        }
+        
+        protected ThemeService ThemeService
+        {
+            get;
+            set;
+        }
+        
+        protected DataBaseManager DataBase
+        {
+            get;
+            set;
+        }
+    }
+}
