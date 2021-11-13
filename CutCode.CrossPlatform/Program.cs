9919d1b4ad65ad44c3f717735acd34098c59e7e5
abdesol <abdesoltak@gmail.com>
2021-11-13 15:34:23 +0300

    done with about design

--- CutCode.CrossPlatform/Program.cs
+++ CutCode.CrossPlatform/Program.cs
@@ -2,6 +2,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 
 namespace CutCode.CrossPlatform
 {
@@ -11,9 +13,14 @@ namespace CutCode.CrossPlatform
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            IconProvider.Register<FontAwesomeIconProvider>();
 
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
