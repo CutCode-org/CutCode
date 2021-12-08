e59f4e25d3a3788e2dd8b9682750a9eff1720e6d
abdesol <abdesoltak@gmail.com>
2021-12-08 14:23:37 +0300

    Custom scroll bar colors fixed

--- CutCode.CrossPlatform/App.axaml.cs
+++ CutCode.CrossPlatform/App.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Drawing;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -12,6 +13,7 @@ using CutCode.CrossPlatform.Views;
 using CutCode.DataBase;
 using ReactiveUI;
 using Splat;
+using Color = Avalonia.Media.Color;
 
 namespace CutCode.CrossPlatform
 {
@@ -26,6 +28,34 @@ namespace CutCode.CrossPlatform
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
+                ThemeService.Current.ThemeChanged += (sender, args) =>
+                {
+                    if (ThemeService.Current.IsLightTheme == true)
+                    {
+                        Application.Current.Resources["ScrollBarTrackFill"] = new SolidColorBrush(Color.Parse("#BCBCBC"), 0.8);
+                        Application.Current.Resources["ScrollBarTrackFillPointerOver"] = new SolidColorBrush(Color.Parse("#BCBCBC"), 0.8);
+                        
+                        Application.Current.Resources["ScrollBarButtonArrowForeground"] = new SolidColorBrush(Colors.Black, 1);
+                        Application.Current.Resources["ScrollBarButtonArrowForegroundPointerOver"] = new SolidColorBrush(Colors.Black, 1);
+                        Application.Current.Resources["ScrollBarButtonArrowForegroundPressed"] = new SolidColorBrush(Colors.Black, 1);
+                        
+                        
+                        Application.Current.Resources["ScrollBarThumbFillPointerOver"] = new SolidColorBrush(Color.Parse("#B3B3B3"), 0.9);
+                        Application.Current.Resources["ScrollBarThumbFillPressed"] = new SolidColorBrush(Color.Parse("#ACACAC"), 0.9);
+                    }
+                    else
+                    {
+                        Application.Current.Resources["ScrollBarTrackFill"] = new SolidColorBrush(Color.Parse("#2A2E33"), 0.8);
+                        Application.Current.Resources["ScrollBarTrackFillPointerOver"] = new SolidColorBrush(Color.Parse("#24272B"), 0.8);
+                        
+                        Application.Current.Resources["ScrollBarButtonArrowForeground"] = new SolidColorBrush(Colors.White, 1);
+                        Application.Current.Resources["ScrollBarButtonArrowForegroundPointerOver"] = new SolidColorBrush(Colors.White, 1);
+                        Application.Current.Resources["ScrollBarButtonArrowForegroundPressed"] = new SolidColorBrush(Colors.White, 1);
+                        
+                        Application.Current.Resources["ScrollBarThumbFillPointerOver"] = new SolidColorBrush(Color.Parse("#393E44"), 0.9);
+                        Application.Current.Resources["ScrollBarThumbFillPressed"] = new SolidColorBrush(Color.Parse("#3E4249"), 0.9);
+                    }
+                };
                 desktop.MainWindow = new MainWindow
                 {
                     DataContext = new MainWindowViewModel()
