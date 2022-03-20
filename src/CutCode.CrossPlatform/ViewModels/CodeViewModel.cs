740c49f7adca40abf6d65fb23d6beed11e54b74f
Sabuto <robe_dunne@hotmail.com>
2022-03-20 14:28:08 +0000

    Add Fody to CodeViewModel.cs

--- src/CutCode.CrossPlatform/ViewModels/CodeViewModel.cs
+++ src/CutCode.CrossPlatform/ViewModels/CodeViewModel.cs
@@ -12,15 +12,16 @@ using CutCode.CrossPlatform.Views;
 using CutCode.CrossPlatform.Services;
 using Newtonsoft.Json;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace CutCode.CrossPlatform.ViewModels
 {
     public class CodeViewModel : PageBaseViewModel
     {
         public CodeModel Code;
-        
+
         public ObservableCollection<CodeCellViewModel?> Cells { get; }
-        
+
         public CodeViewModel(CodeModel code)
         {
             Code = code;
@@ -28,15 +29,15 @@ namespace CutCode.CrossPlatform.ViewModels
             Language = code.Language;
 
             IsEditEnabled = false;
-            
+
             var cellsDict = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(code.Cells);
             Cells = new ObservableCollection<CodeCellViewModel?>();
             CellsToViewModel(cellsDict);
-            for(int i = 0; i < Cells.Count; i++)
+            for (int i = 0; i < Cells.Count; i++)
             {
                 Cells[i].IsEditable = false;
             }
-            
+
             IsCellEmpty = Cells.Count == 0;
             Cells.CollectionChanged += (sender, args) =>
             {
@@ -44,23 +45,22 @@ namespace CutCode.CrossPlatform.ViewModels
                 if (IsCellEmpty && !IsEditEnabled)
                 {
                     IsEditEnabled = IsCellEmpty;
-                    for(int i = 0; i < Cells.Count; i++)
+                    for (int i = 0; i < Cells.Count; i++)
                     {
                         Cells[i].IsEditable = IsEditEnabled;
                     }
                 }
-                    
             };
-            
-            if(ThemeService.Theme == ThemeType.Light) OnLightThemeIsSet();
+
+            if (ThemeService.Theme == ThemeType.Light) OnLightThemeIsSet();
             else OnDarkThemeIsSet();
-            
+
             ThemeService.ThemeChanged += (sender, args) =>
             {
-                if(ThemeService.Theme == ThemeType.Light) OnLightThemeIsSet();
+                if (ThemeService.Theme == ThemeType.Light) OnLightThemeIsSet();
                 else OnDarkThemeIsSet();
             };
-            
+
             IsFavouritePath = code.IsFavourite ? IconPaths.StarFull : IconPaths.Star;
         }
 
@@ -70,145 +70,69 @@ namespace CutCode.CrossPlatform.ViewModels
             if (cells == null) return;
             foreach (var cell in cells)
             {
-                Cells.Add(new CodeCellViewModel(this,cell["Description"], cell["Code"]));
+                Cells.Add(new CodeCellViewModel(this, cell["Description"], cell["Code"]));
             }
         }
 
         private void OnLightThemeIsSet()
         {
-            BackgroundColor =  Color.Parse("#FCFCFC");
-            BarBackground =  Color.Parse("#F6F6F6");
-            
+            BackgroundColor = Color.Parse("#FCFCFC");
+            BarBackground = Color.Parse("#F6F6F6");
+
             TextAreaBackground = Color.Parse("#ECECEC");
             TextAreaForeground = Color.Parse("#000000");
             TextAreaOverlayBackground = Color.Parse("#E2E2E2");
-            
+
             ComboBoxBackground = Color.Parse("#ECECEC");
             ComboBoxBackgroundOnHover = Color.Parse("#E2E2E2");
-            
+
             BtnColor = Color.Parse("#090909");
             IsFavouriteColor = Code.IsFavourite ? Color.Parse("#F7A000") : Color.Parse("#4D4D4D");
         }
 
         private void OnDarkThemeIsSet()
         {
-            BackgroundColor =  Color.Parse("#36393F");
-            BarBackground =  Color.Parse("#303338");
-            
+            BackgroundColor = Color.Parse("#36393F");
+            BarBackground = Color.Parse("#303338");
+
             TextAreaBackground = Color.Parse("#2A2E33");
             TextAreaForeground = Color.Parse("#FFFFFF");
             TextAreaOverlayBackground = Color.Parse("#24272B");
-            
+
             ComboBoxBackground = Color.Parse("#2A2E33");
             ComboBoxBackgroundOnHover = Color.Parse("#24272B");
-            
+
             BtnColor = Color.Parse("#F2F2F2");
             IsFavouriteColor = Code.IsFavourite ? Color.Parse("#F7A000") : Color.Parse("#94969A");
         }
-        
-        private string _title;
-        public string Title
-        {
-            get => _title;
-            set => this.RaiseAndSetIfChanged(ref _title, value);
-        }
-        
-        private bool _isCellEmpty;
 
-        public bool IsCellEmpty
-        {
-            get => _isCellEmpty;
-            set => this.RaiseAndSetIfChanged(ref _isCellEmpty, value);
-        }
+        [Reactive] public string Title { get; set; }
 
-        private bool _isEditEnabled;
+        [Reactive] public bool IsCellEmpty { get; set; }
 
-        public bool IsEditEnabled
-        {
-            get => _isEditEnabled;
-            set => this.RaiseAndSetIfChanged(ref _isEditEnabled, value);
-        }
-        
-        private Color _backgroundColor;
+        [Reactive] public bool IsEditEnabled { get; set; }
 
-        public Color BackgroundColor
-        {
-            get => _backgroundColor;
-            set => this.RaiseAndSetIfChanged(ref _backgroundColor, value);
-        }
-        
-        private Color _btnColor;
-        public Color BtnColor
-        {
-            get => _btnColor;
-            set =>  this.RaiseAndSetIfChanged(ref _btnColor, value);
-        }
-        
-        private Color _comboBoxBackground;
-        public Color ComboBoxBackground
-        {
-            get => _comboBoxBackground;
-            set =>this.RaiseAndSetIfChanged(ref _comboBoxBackground, value);
-            
-        }
-        
-        private Color _comboBoxBackgroundOnHover;
-        public Color ComboBoxBackgroundOnHover
-        {
-            get => _comboBoxBackgroundOnHover;
-            set =>this.RaiseAndSetIfChanged(ref _comboBoxBackgroundOnHover, value);
-            
-        }
-        
-        private Color _barBackground;
+        [Reactive] public Color BackgroundColor { get; set; }
 
-        public Color BarBackground
-        {
-            get => _barBackground;
-            set => this.RaiseAndSetIfChanged(ref _barBackground, value);
-        }
+        [Reactive] public Color BtnColor { get; set; }
 
-        private Color _textAreaBackground;
-        public Color TextAreaBackground
-        {
-            get => _textAreaBackground;
-            set => this.RaiseAndSetIfChanged(ref _textAreaBackground, value);
-        }
+        [Reactive] public Color ComboBoxBackground { get; set; }
 
-        private Color _textAreaForeground;
-        public Color TextAreaForeground
-        {
-            get => _textAreaForeground;
-            set => this.RaiseAndSetIfChanged(ref _textAreaForeground, value);
-        }
-        
-        private Color _textAreaOverlayBackground;
-        public Color TextAreaOverlayBackground
-        {
-            get => _textAreaOverlayBackground;
-            set => this.RaiseAndSetIfChanged(ref _textAreaOverlayBackground, value);
-        }
-        
-        private Color _isFavouriteColor;
-        public Color IsFavouriteColor
-        {
-            get => _isFavouriteColor;
-            set => this.RaiseAndSetIfChanged(ref _isFavouriteColor, value);
-        }
-        
-        private string _isFavouritePath;
-        public string IsFavouritePath
-        {
-            get => _isFavouritePath;
-            set => this.RaiseAndSetIfChanged(ref _isFavouritePath, value);
-        }
-        
-        private string _language;
-        public string Language
-        {
-            get => _language;
-            set => this.RaiseAndSetIfChanged(ref _language, value);
-        }
+        [Reactive] public Color ComboBoxBackgroundOnHover { get; set; }
+
+        [Reactive] public Color BarBackground { get; set; }
+
+        [Reactive] public Color TextAreaBackground { get; set; }
+
+        [Reactive] public Color TextAreaForeground { get; set; }
+
+        [Reactive] public Color TextAreaOverlayBackground { get; set; }
+
+        [Reactive] public Color IsFavouriteColor { get; set; }
+
+        [Reactive] public string IsFavouritePath { get; set; }
+
+        [Reactive] public string Language { get; set; }
 
         public async void AddCell()
         {
@@ -226,13 +150,13 @@ namespace CutCode.CrossPlatform.ViewModels
                 !Cells.Select(x => x.Description).ToList().Any(string.IsNullOrEmpty) &&
                 !Cells.Select(x => x.Code).ToList().Any(string.IsNullOrEmpty))
             {
-                var cellsList = Cells.Select(x => 
+                var cellsList = Cells.Select(x =>
                     new Dictionary<string, string>()
                     {
-                        {"Description", x.Description},
-                        {"Code", x.Code}
+                        { "Description", x.Description },
+                        { "Code", x.Code }
                     }).ToList();
-            
+
                 var editedCode = new CodeModel(Title, cellsList, Language,
                     new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(), Code.IsFavourite);
                 editedCode.SetId(Code.Id);
@@ -240,7 +164,7 @@ namespace CutCode.CrossPlatform.ViewModels
                 if (DataBase.EditCode(editedCode))
                 {
                     IsEditEnabled = false;
-                    for(int i = 0; i < Cells.Count; i++)
+                    for (int i = 0; i < Cells.Count; i++)
                     {
                         Cells[i].IsEditable = false;
                     }
@@ -259,7 +183,7 @@ namespace CutCode.CrossPlatform.ViewModels
         public async void EditCommand()
         {
             IsEditEnabled = true;
-            for(int i = 0; i < Cells.Count; i++)
+            for (int i = 0; i < Cells.Count; i++)
             {
                 Cells[i].IsEditable = true;
             }
@@ -272,15 +196,15 @@ namespace CutCode.CrossPlatform.ViewModels
             {
                 Code.IsFavourite = !Code.IsFavourite;
                 IsFavouritePath = Code.IsFavourite ? IconPaths.StarFull : IconPaths.Star;
-            
-                if(ThemeService.Current.Theme == ThemeType.Light) IsFavouriteColor = Code.IsFavourite ? Color.Parse("#F7A000") : Color.Parse("#4D4D4D");
+
+                if (ThemeService.Current.Theme == ThemeType.Light)
+                    IsFavouriteColor = Code.IsFavourite ? Color.Parse("#F7A000") : Color.Parse("#4D4D4D");
                 else IsFavouriteColor = Code.IsFavourite ? Color.Parse("#F7A000") : Color.Parse("#94969A");
             }
             else
             {
                 NotificationService.CreateNotification("Error", "Error, Unable to save the changes!", 3);
             }
-            
         }
 
         public async void DeleteCode()
@@ -304,7 +228,7 @@ namespace CutCode.CrossPlatform.ViewModels
 
         public static void DeleteCell(CodeViewModel vm, CodeCellViewModel cell)
         {
-            if(vm.IsEditEnabled) vm.Cells.Remove(cell);
+            if (vm.IsEditEnabled) vm.Cells.Remove(cell);
         }
     }
 }
