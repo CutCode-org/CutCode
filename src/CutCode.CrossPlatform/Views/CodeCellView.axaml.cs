729513401865b6d92c6d35ee9265f7c4cb639204
Sabuto <robe_dunne@hotmail.com>
2022-03-24 20:48:11 +0000

    remove old projects, and add nuget package

--- src/CutCode.CrossPlatform/Views/CodeCellView.axaml.cs
+++ src/CutCode.CrossPlatform/Views/CodeCellView.axaml.cs
@@ -1,14 +1,24 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using AvaloniaEdit;
+using CutCode.CrossPlatform.ViewModels;
+using ReactiveUI;
 
 namespace CutCode.CrossPlatform.Views
 {
-    public class CodeCellView : UserControl
+    public class CodeCellView : ReactiveUserControl<CodeCellViewModel>
     {
+        private TextEditor TextEditor => this.FindControl<TextEditor>(nameof(TextEditor));
+
         public CodeCellView()
         {
             InitializeComponent();
+            this.WhenActivated(d =>
+            {
+                this.Bind(ViewModel, x => x.Code, x => x.TextEditor.Text);
+            });
         }
 
         private void InitializeComponent()
