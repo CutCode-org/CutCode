a96a3875bb2704bb2ada458bfd6e487ef8be8f25
Sabuto <robe_dunne@hotmail.com>
2022-03-25 16:54:27 +0000

    progress checkin

--- src/CutCode.CrossPlatform/Views/CodeCellView.axaml.cs
+++ src/CutCode.CrossPlatform/Views/CodeCellView.axaml.cs
@@ -4,6 +4,8 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using AvaloniaEdit;
+using AvaloniaEdit.TextMate;
+using AvaloniaEdit.TextMate.Grammars;
 using CutCode.CrossPlatform.ViewModels;
 using ReactiveUI;
 
@@ -12,11 +14,21 @@ namespace CutCode.CrossPlatform.Views
     public class CodeCellView : ReactiveUserControl<CodeCellViewModel>
     {
         private TextEditor TextEditor => this.FindControl<TextEditor>(nameof(TextEditor));
+        private readonly TextMate.Installation _textMateInstallation;
+        private RegistryOptions _registryOptions;
 
         public CodeCellView()
         {
             InitializeComponent();
             this.WhenActivated(d => { this.Bind(ViewModel, x => x.Code, x => x.TextEditor.Text).DisposeWith(d); });
+
+            _registryOptions = new RegistryOptions(ThemeName.Dark);
+            _textMateInstallation = TextEditor.InstallTextMate(_registryOptions);
+
+            Language csharpLanguage = _registryOptions.GetLanguageByExtension(".cs");
+
+            string scopeName = _registryOptions.GetScopeByLanguageId(csharpLanguage.Id);
+            _textMateInstallation.SetGrammar(scopeName);
         }
 
         private void InitializeComponent()
