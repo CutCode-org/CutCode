5d495274afa9041bce88831c73bfd10903895ff7
Sabuto <robe_dunne@hotmail.com>
2022-04-15 15:45:40 +0100

    updated animated listbox to work on adding to visualtree

--- src/CutCode.CrossPlatform/Controls/AnimatedListBoxItem.cs
+++ src/CutCode.CrossPlatform/Controls/AnimatedListBoxItem.cs
@@ -1,25 +1,31 @@
 ﻿using System;
 using System.Diagnostics;
 using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
 using Avalonia.Input;
+using Avalonia.LogicalTree;
 using Avalonia.Styling;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace CutCode.CrossPlatform.Controls;
 
 [PseudoClasses(":Added", ":Removed")]
 public class AnimatedListBoxItem : ListBoxItem, IStyleable
 {
+    public AnimatedListBoxItem()
+    {
+    }
     Type IStyleable.StyleKey => typeof(AnimatedListBoxItem);
 
     private bool isAttached = false;
     protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
     {
-        base.OnAttachedToVisualTree(e);
         if (isAttached) return;
         PseudoClasses.Set(":Added", true);
         isAttached = true;
+        base.OnAttachedToVisualTree(e);
     }
 
     protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
